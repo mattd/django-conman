34cb6a350240c66cfe4bee824e5e1b6e1f7ca1ca
Matt Dawson <matthewtdawson@gmail.com>
2010-05-04 22:56:40 -0400

    Adding tinymce

--- conman/admin.py
+++ conman/admin.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
 
 from conman.models import Page
 
 class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+
+    class Media:
+        try:
+            js = settings.TINYMCE_JS_FILES
+        except AttributeError:
+            pass
 
 admin.site.register(Page, PageAdmin)
